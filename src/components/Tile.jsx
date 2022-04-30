8a2658f92d41bf0653f57ec7f2d6aa28f74455f6
andrewbrindley <a.brindley@hotmail.com>
2022-04-30 19:41:18 +1000

    updated board

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,21 +1,34 @@
 import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
+import star from '../star.png';
+
+const COLOURS = {
+    '3L': '#000080',
+    '3W': '#990000',
+    '2L': '#ADD8E6',
+    '2W': '#FFD580',
+
+}
 
 const StyledTile = styled.div`
-    background: grey;
+    background: #D3D3D3;
     display: flex;
     justify-content: center;
     align-items: center;
-    background: #E0E0B0;
+    background: ${p => p.color};
+    color: white;
     border: 1px solid white;
+    > img{
+        width: 100%;
+        height: 100%;
+    }
 `;
 
 export const Tile = ({el}) => {
+    const color = el in COLOURS ? COLOURS[el] : '#D3D3D3'; 
     return (
-        <StyledTile>
-            <span>
-                {el}
-            </span>
+        <StyledTile color = {color}>
+            {el === 'STAR' ? <img src={star}/> : <span> {el} </span>}        
         </StyledTile>
     )
 }
